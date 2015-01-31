db5a927a48fe24a94d0aaf50788d20ba2d321ee9
Alexander von Gluck IV <kallisti5@unixzen.com>
2015-01-31 17:37:01 -0600

    Cleanup: Tighten up usage, clean up system calls
    * Open3 runs a lot tighter
    * Add optional color support
    * Add helper functions
    * Ascii logos are neat.

--- ruby/client.rb
+++ ruby/client.rb
@@ -1,6 +1,12 @@
 #!/bin/ruby
 
-@version = "0.1"
+# Haikeuken buildslave script
+##############################################
+# Copyright 2014 - 2015 Alexander von Gluck IV
+# Released under the terms of the GPLv3
+##############################################
+
+@version = "0.2"
 @rest_period = 5.0
 
 ##########################################
@@ -9,14 +15,80 @@
 require 'json'
 require 'net/http'
 require 'yaml'
-#require 'colorize'
+require 'open3'
+
+if Gem::Specification::find_all_by_name('colorize').any?
+	require 'colorize'
+	@color_support = true
+else
+	@color_support = false
+end
 ##########################################
 
-# Pull settings from B_USER_SETTINGS_DIRECTORY/haikeuken.yml
-settings_dir = `finddir B_USER_SETTINGS_DIRECTORY`.delete!("\n")
-@settings = YAML::load_file("#{settings_dir}/haikeuken.yml")
-@remote_uri = "#{@settings['server']['url']}/builders/#{@settings['general']['hostname']}"
 
+##########################################
+# Helper functions
+def warning(message)
+	if @color_support
+		puts "Warning: #{message}".colorize(:yellow)
+	else
+		puts "Warning: #{message}"
+	end
+end
+
+def error(message)
+	if @color_support
+		puts "Error: #{message}".colorize(:red)
+	else
+		puts "Error: #{message}"
+	end
+	exit 1
+end
+#
+##########################################
+
+
+puts ' _   _       _ _              _               '
+puts '| | | |     (_) |            | |              '
+puts '| |_| | __ _ _| | _____ _   _| | _____ _ __   '
+puts '|  _  |/ _` | | |/ / _ \ | | | |/ / _ \ \'_ \ '
+puts '| | | | (_| | |   <  __/ |_| |   <  __/ | | | '
+puts '\_| |_/\__,_|_|_|\_\___|\__,_|_|\_\___|_| |_| '
+puts '--------------------------------------------- '
+puts '         The Haiku package builder            '
+puts "                Version #{@version}"
+puts ''
+
+# Load some basic platform info
+hostname, status_host = Open3.capture2e("uname -n")
+platform, status_os = Open3.capture2e("uname -s")
+architecture, status_arch = Open3.capture2e("uname -m")
+
+if status_host && status_os && status_arch
+	# Clean up vars
+	hostname.delete!("\n")
+	platform.delete!("\n")
+	architecture.delete!("\n")
+	puts "Starting buildslave on #{hostname} (#{platform}, #{architecture})"
+else
+	error("Error pulling machine information!")
+end
+
+begin
+	if platform == "Linux"
+		settings_dir, status = Open3.capture2e("echo -n ~")
+		warning("Running on Linux. We don't support non-native platforms yet.")
+	elsif platorm == "Haiku"
+		settings_dir, status = Open3.capture2e("finddir", "B_USER_SETTINGS_DIRECTORY")
+		settings_dir.delete!("\n")
+	end
+	# Load settings YAML
+	@settings = YAML::load_file("#{settings_dir}/haikeuken.yml")
+rescue
+	error("Problem loading configuration file at #{settings_dir}/haikeuken.yml")
+end
+
+@remote_uri = "#{@settings['server']['url']}/builders/#{hostname}"
 @porter_arguments = "-y -v --no-dependencies"
 
 #puts @settings.inspect
@@ -38,15 +110,12 @@ end
 def putwork(status, buildlog, build_id)
 	uri = URI("#{@remote_uri}/putwork")
 
-	#req = Net::HTTP::Post.new(uri.request_uri)
-	#req.set_form_data('result' => result)
-
-	log = File.open(buildlog, "rb")
+	#log = File.open(buildlog, "rb")
 
 	begin
 		Net::HTTP.post_form uri, {"token" => @settings['general']['token'],
 			"status" => status, "build_id" => build_id,
-			"result" => log.read}
+			"result" => buildlog}
 		#Net::HTTP.start(uri.hostname, uri.port) do |http|
 		#	http.request(req)
 		#end
@@ -54,10 +123,10 @@ def putwork(status, buildlog, build_id)
 		puts "=========================================="
 		puts "Error: Server #{uri}"
 		puts "=========================================="
-		log.close
+		#log.close
 		return nil
 	end
-	log.close
+	#log.close
 end
 
 
@@ -104,9 +173,11 @@ def loop()
 	
 	work.each do |task|
 		puts "+ Work received"
-		worklog = "/tmp/#{task['name']}-#{task['version']}-#{task['revision']}.log"
+		#worklog = "/tmp/#{task['name']}-#{task['version']}-#{task['revision']}.log"
 		puts "+ Building #{task['name']}-#{task['version']}-#{task['revision']}"
-		result = system "#{@settings['general']['work_path']}/haikuporter/haikuporter #{@porter_arguments} #{task['name']}-#{task['version']} &> #{worklog}"
+		worklog, result = Open3.capture2e("#{@settings['general']['work_path']}/haikuporter/haikuporter",
+			@porter_arguments, "#{task['name']}-#{task['version']}")
+		#result = system "#{@settings['general']['work_path']}/haikuporter/haikuporter #{@porter_arguments} #{task['name']}-#{task['version']} &> #{worklog}"
 		status = result ? "OK" : "Fail"
 		putwork(status, worklog, task['id'])
     end
